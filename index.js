a18a43d51988f369cbbc879d1fb16f0c3e18002b
bayaraa <bayarsuren0310@gmail.com>
2023-05-25 00:18:31 +0800

    ajja

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 
 // Connect to MongoDB
 mongoose.set('strictQuery', true);
-mongoose.connect('mongodb+srv://bayarsurendev:02atau1s1qTy0ukl@cluster0.kif9fm4.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect(process.env.DATABASE, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverSelectionTimeoutMS: 5000, // Increase timeout if necessary
@@ -21,7 +21,6 @@ mongoose.connect('mongodb+srv://bayarsurendev:02atau1s1qTy0ukl@cluster0.kif9fm4.
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/user');
 const cargoRoutes = require('./routes/cargo');
-const upload = require('./routes/upload');
 const order = require('./routes/order');
 const adminRoutes = require('./routes/admin');
 const ratingRoutes = require('./routes/rating');
@@ -31,21 +30,20 @@ const sitesRoutes = require('./routes/sites');
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
-// if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === 'development') {
   app.use(cors({origin: ['http://localhost:3000', 'http://localhost:3001', 'https://ecargo.vercel.app']}));
-// }
+}
 
 // Middlewares
 app.use('/api', authRoutes);
 app.use('/api', userRoutes);
 app.use('/api', cargoRoutes);
-app.use('/api', upload);
 app.use('/api', order);
 app.use('/api', adminRoutes);
 app.use('/api', ratingRoutes);
 app.use('/api', sitesRoutes);
 
 const port = process.env.PORT || 5000;
-app.listen(5000, () => {
-  console.log(`Server running on port`);
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
--- routes/upload.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { uploads, read } = require('../controllers/upload');
-const router = express.Router()
-
-router.post('/upload/image',uploads);
-router.get('/haha/:file', read);
-
-module.exports = router;
