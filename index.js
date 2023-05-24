5a7e209070ced6755a6965248d970bd7419f540e
bayaraa <bayarsuren0310@gmail.com>
2023-05-24 23:45:01 +0800

    ajja

--- index.js
+++ index.js
@@ -6,49 +6,49 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 
 const app = express();
-const DATABASE='mongodb+srv://bayarsurendev:02atau1s1qTy0ukl@cluster0.kif9fm4.mongodb.net/?retryWrites=true&w=majority'
-// connect mongodb
-mongoose.set('strictQuery', true);
-mongoose.connect(DATABASE, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    serverSelectionTimeoutMS: 5000, // Increase timeout if necessary
-  })
-  .then(() => console.log('DB connected'))
-  .catch(err => console.log('DB ERROR', err));
+// const DATABASE='mongodb+srv://bayarsurendev:02atau1s1qTy0ukl@cluster0.kif9fm4.mongodb.net/?retryWrites=true&w=majority'
+// // connect mongodb
+// mongoose.set('strictQuery', true);
+// mongoose.connect(DATABASE, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     serverSelectionTimeoutMS: 5000, // Increase timeout if necessary
+//   })
+//   .then(() => console.log('DB connected'))
+//   .catch(err => console.log('DB ERROR', err));
   
 
-//import routes
-const authRoutes = require('./routes/auth')
-const userRoutes = require('./routes/user')
-const cargoRoutes = require('./routes/cargo')
-const upload = require('./routes/upload')
-const order = require('./routes/order')
-const adminRoutes = require('./routes/admin')
-const ratingRoutes = require('./routes/rating')
-const sitesRoutes = require('./routes/sites')
+// //import routes
+// const authRoutes = require('./routes/auth')
+// const userRoutes = require('./routes/user')
+// const cargoRoutes = require('./routes/cargo')
+// const upload = require('./routes/upload')
+// const order = require('./routes/order')
+// const adminRoutes = require('./routes/admin')
+// const ratingRoutes = require('./routes/rating')
+// const sitesRoutes = require('./routes/sites')
 
-//app midllewares
-app.use(morgan('dev'));
-app.use(bodyParser.json());
-const NODE_ENV= 'development'
+// //app midllewares
+// app.use(morgan('dev'));
+// app.use(bodyParser.json());
+// const NODE_ENV= 'development'
 
-if(NODE_ENV == 'development'){
-    app.use(cors({origin: [`http://localhost:3000`,`http://localhost:3001`, `https://ecargo.vercel.app`]}));
-}
+// if(NODE_ENV == 'development'){
+//     app.use(cors({origin: [`http://localhost:3000`,`http://localhost:3001`, `https://ecargo.vercel.app`]}));
+// }
 
 
-//midlewares
-app.use('/api', authRoutes); 
-app.use('/api', userRoutes);
-app.use('/api', cargoRoutes);
-app.use('/api', upload);
-app.use('/api', order);
-app.use('/api', adminRoutes);
-app.use('/api', ratingRoutes)
-app.use('/api', sitesRoutes)
-
-const port = process.env.PORT || 5000
+// //midlewares
+// app.use('/api', authRoutes); 
+// app.use('/api', userRoutes);
+// app.use('/api', cargoRoutes);
+// app.use('/api', upload);
+// app.use('/api', order);
+// app.use('/api', adminRoutes);
+// app.use('/api', ratingRoutes)
+// app.use('/api', sitesRoutes)
+const PORT = 5000
+const port = PORT || 5000
 app.listen(port, () => {
     console.log(`port ${port}`)
 })
